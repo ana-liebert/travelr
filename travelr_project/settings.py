0199fa1b9a675e58e7c09035f329291d0ecac8f3
Ana Liebert <analiebert19@gmail.com>
2022-03-30 15:45:44 -0400

    additions for heroku deployment

--- travelr_project/settings.py
+++ travelr_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "starter"
+
+django_heroku.settings(locals())
